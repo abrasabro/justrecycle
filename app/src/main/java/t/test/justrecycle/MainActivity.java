7a546ac415333daa5601e01a1960b059761b8a13
abrasabro <abrasabro@hotmail.com>
2018-01-31 20:01:27 -0600

    initial commit

---
+++ app/src/main/java/t/test/justrecycle/MainActivity.java
@@ -0,0 +1,41 @@
+package t.test.justrecycle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private ArrayList<String> mNames;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: started");
+        initTexts();
+        initRecyclerView();
+    }
+
+    private void initTexts(){
+        mNames.add("asdfgag");
+        mNames.add("343tdfg");
+        mNames.add("erter");
+        mNames.add("tuoyu");
+        mNames.add("aergaer");
+        mNames.add("sdfghsdgh");
+    }
+
+    private void initRecyclerView(){
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(mNames, this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
---
+++ app/src/main/java/t/test/justrecycle/RecyclerViewAdapter.java
@@ -0,0 +1,63 @@
+package t.test.justrecycle;
+
+import android.content.Context;
+import android.support.constraint.ConstraintLayout;
+import android.text.Layout;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+
+    private static final String TAG = "RecyclerViewAdapter";
+    private ArrayList<String> mTextList = new ArrayList<>();
+    private Context mContext;
+
+    public RecyclerViewAdapter(ArrayList<String> mTextList, Context mContext) {
+        this.mTextList = mTextList;
+        this.mContext = mContext;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_listitem, parent, false);
+        RecyclerView.ViewHolder holder = new ViewHolder(view);
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
+        Log.d(TAG, "onBindViewHolder: called");
+        ((TextView)holder.itemView).setText(mTextList.get(position));
+        ((ViewHolder)holder).parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: clicked " + mTextList.get(position));
+                Toast.makeText(mContext, mTextList.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTextList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView textView;
+        ConstraintLayout parentLayout;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.textview);
+            parentLayout = itemView.findViewById(R.id.parentlayout);
+        }
+    }
+}
